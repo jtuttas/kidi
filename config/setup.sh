d5c2cf6942be11a1994cbaf12b20dd44775c674c
jtuttas <jtuttas@gmx.net>
2024-06-17 18:03:07 +0200

    mit ok setup.sh

--- config/setup.sh
+++ config/setup.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
 # Setze die URL des Git-Repositories und das Zielverzeichnis
-REPO_URL="https://github.com/dein-benutzername/dein-repository.git"
-TARGET_DIR="/pfad/zu/deinem/verzeichnis"
+REPO_URL="https://github.com/jtuttas/kidi.git"
+TARGET_DIR="/home/tuttas/kidi"
+
+# Setze das Home-Verzeichnis
+export HOME="/root"
 
 # Überprüfe, ob das Verzeichnis existiert
 if [ -d "$TARGET_DIR" ]; then
   echo "Verzeichnis existiert. Führe git pull aus."
   cd "$TARGET_DIR"
+  git config --global --add safe.directory "$TARGET_DIR"
   git pull
 else
   echo "Verzeichnis existiert nicht. Klone das Repository."
   git clone "$REPO_URL" "$TARGET_DIR"
 fi
+
+
+# Benutzername und Passwort
+username="schueler1"
+password="kidi"
+
+# Benutzer löschen
+sudo userdel -r $username
+
+# Benutzer neu anlegen
+sudo useradd -m -s /bin/bash $username
+
+# Passwort setzen
+echo "$username:$password" | sudo chpasswd
+
+# Benutzer zu zusätzlichen Gruppen hinzufügen (falls nötig)
+# sudo usermod -aG group1,group2 $username
+
+echo "Benutzer $username wurde gelöscht, neu angelegt und das Passwort gesetzt."
+
+
+cp -r "$TARGET_DIR"/notebooks/* /home/schueler1/
